af70f609c50222e8d9b8b37462d4c7235b14770e
Norwace <simen.os@outlook.com>
2026-06-19 23:02:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "anleggsservice-v13";
+const CACHE = "anleggsservice-v26";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
